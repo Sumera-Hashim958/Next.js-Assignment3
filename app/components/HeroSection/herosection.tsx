87c042c782e82319fdc4499aebfab899c80fcc47
Yousuftayyaba <Yousuftayyaba2@gmail.com>
2024-10-21 20:42:09 +0500

    Thirs Assignment given by Sir Hamza Alvi

---
+++ app/components/HeroSection/herosection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className="border-4 border-blue-900 border-dotted bg-gradient-to-r from-green-500 via-purple-500 to-green-500 py-16 text-center">
+      {/* <h1 className="text-4xl font-bold mb-4"> This is Hero Section</h1> */}
+       <h2 className="text-4xl font-bold mb-4">Hello, My name is Sumera Hashim</h2>
+  <p className="text-lg mb-8">My projects showcasing my growing skills in JavaScript, TypeScript, HTML, and CSS. The real-time updating feature enhances user experience, while the responsive design ensures it looks great on any device. By building this project, I've been able to apply the concepts I've learned in React and explore the powerful capabilities of Next.js, further enriching my web development journey.</p>
+  </section>
+  )
+}
+
+export default HeroSection
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer";
+import HeroSection from "./components/HeroSection/herosection";
+import ProjectsPage from "./components/projects/projects";
+import AboutPage from "./components/about/about";
+import ContactPage from "./components/contact/contact";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +31,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <section>
+        <Header/>
+        </section>
+
+        <section className="my-4">
+        <HeroSection/>
+        </section>
+
+        <section className="my=4">
+        <ProjectsPage/>
+        </section>
+
+        <section className="my-4">
+        <AboutPage/>
+        </section>
+
+        <section className="my-4">
+        <ContactPage/>
+        </section>
+        <Footer/>
+        {/* {children} */}
+      
       </body>
     </html>
   );
